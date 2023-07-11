c61cf017208c666bee0225ed40c478d91cc260e2
Elom Agboka <opentech.consult@gmail.com>
2023-07-11 19:22:41 +0000

    extending the web server. This version of the web server can handle request correctly.

--- server.mjs
+++ server.mjs
@@ -1,6 +1,10 @@
 import { createServer } from 'http';
 
-const server = createServer();
+const server = createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/plain; charset-utf8'});
+    res.write('Hello ');
+    res.end(' World\n');
+});
 
 server.listen(8080, () => {
     console.log(`Server is listening to http://localhost:${server.address().port}`);
