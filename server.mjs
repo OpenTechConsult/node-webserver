aff46c89cd7a58c792edc87cd38ada1da4ba6683
Elom Agboka <opentech.consult@gmail.com>
2023-07-11 20:09:46 +0000

    extend the webserver so that it responds with a response in HTML

--- server.mjs
+++ server.mjs
@@ -1,9 +1,18 @@
 import { createServer } from 'http';
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/plain; charset-utf8'});
-    res.write('Hello ');
-    res.end(' World\n');
+    res.writeHead(200, {'content-type': 'text/html; charset-utf8'});
+    const body = `<!DOCTYPE html>
+    <html>
+        <head>
+        <meta charset="utf-8">
+        <title>Node.js Demo</title>
+        </head>
+        <body>
+        <h1 style="color:green">Hello World</h1>
+        </body>
+    </html>`;
+    res.end(body);
 });
 
 server.listen(8080, () => {
