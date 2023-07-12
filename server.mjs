f5e60b97dbf175a86ff2da58b26ae7ce7c20c50a
Elom Agboka <opentech.consult@gmail.com>
2023-07-12 16:00:41 +0000

    remove all the console.log() statements included for debugging purpose.
    Also, we install nodemon

--- server.mjs
+++ server.mjs
@@ -1,13 +1,9 @@
 import { createServer } from 'http';
 
-console.log('createServer')
 const server = createServer((req, res) => {
-    console.log('createServer callback');
 
     res.writeHead(200, {'content-type': 'text/html; charset-utf8'});
     const url = new URL(req.url, 'http://localhost:8080');
-    console.log(url);
-    console.log('Name ', url.searchParams.get('name'));
     const body = `<!DOCTYPE html>
     <html lang="en">
         <head>
@@ -21,7 +17,6 @@ const server = createServer((req, res) => {
     res.end(body);
 });
 
-console.log('listen');
 server.listen(8080, () => {
     console.log(`Server is listening to http://localhost:${server.address().port}`);
 });
